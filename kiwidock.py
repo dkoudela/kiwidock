1cad242736a929408142ae831624ea85b838b8c8
David Koudela <david@koudelovi.cz>
2018-05-11 22:45:18 +0200

    Improved index page.

--- kiwidock.py
+++ kiwidock.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import render_template
+from os import path
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "I want to attend the Kiwi.com Weekend in the Cloud"
+    return render_template('index.html', icons='static')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
