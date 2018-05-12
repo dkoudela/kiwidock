b559067f290a2c058d0a31ff490951cd37e78468
David Koudela <david@koudelovi.cz>
2018-05-12 16:42:26 +0200

    Refactoring: constants & css.

--- kiwidock.py
+++ kiwidock.py
@@ -3,9 +3,12 @@ from flask import render_template
 from os import path
 app = Flask(__name__)
 
+FLASK_STATIC_PATH='static'
+ICONS_PATH=FLASK_STATIC_PATH
+
 @app.route("/")
 def index():
-    return render_template('index.html', icons='static')
+    return render_template('index.html', icons=ICONS_PATH)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
